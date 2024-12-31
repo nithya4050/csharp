dd9d5fddf4a434cc0b4c64f31d7265911ba2255b
nithya4050 <nithyasubramaniandpm@gmail.com>
2024-12-31 13:11:29 +0530

    colours

---
+++ colour/colour/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace colour
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void R_MouseEnter(object sender, MouseEventArgs e)
+        {
+             
+        }
+
+        private void R_MouseLeave(object sender, MouseEventArgs e)
+        {
+           
+
+        }
+
+        private void G_MouseEnter(object sender, MouseEventArgs e)
+        {
+            
+
+        }
+    }
+}
